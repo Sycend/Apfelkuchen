4d143bfcaf0b1f90277ec794bc6a6bea31a9b7a4
Dominik H <databasev@yahoo.de>
2014-11-14 21:47:03 +0100

    minor update for Testing.java

--- Apfelkuchen/src/test/Testing.java
+++ Apfelkuchen/src/test/Testing.java
@@ -1,22 +1,49 @@
 package test;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
+import org.junit.Test;
 import GUI.UnitTable;
 
 /**
- * @author Dominik Hofmann, Mark L
- * @version 1.0.0
+ * @author Dominik Hofmann, Mark Leibmann
+ * @version 1.0.1
  */
 public class Testing {
+	
+	@Test
+	public static void TestBattery(){
+		assertEquals(3, Test0(3));
+		assertEquals(7200, Test1(2));
+		assertEquals(3, Test2(3));
+		assertEquals(7200, Test3(2));
+		System.out.println("All Tests Passed.");
+	}
+	
 	public static int Test0(int n) {
 		UnitTable unitT = new UnitTable();
 		unitT.setEnergy_J_high(n);
-		//energy_J_high = n;
-		//return 0;
-		return unitT.getEnergy_J_high() * unitT.getEnergy_J_gradient();
+		return unitT.SI_high_energy_J();
 	}
 	
-	public static void testEnergy_J() {
-		//assertEquals(0, Test0(3));
+	public static int Test1(int n) {		
+		UnitTable unitT = new UnitTable();
+		unitT.setTime_h_high(n);
+		return unitT.SI_high_time_h();
 	}
+	public static int Test2(int n) {
+		UnitTable unitT = new UnitTable();
+		unitT.setEnergy_J_low(n);
+		return unitT.SI_low_energy_J();
+	}
+	
+	public static int Test3(int n) {		
+		UnitTable unitT = new UnitTable();
+		unitT.setTime_h_low(n);
+		return unitT.SI_low_time_h();
+	}
+	
 }
+
+
+
+
