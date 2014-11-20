fcdbd49d0aca1ae570d1f153aac0d07d20d0d569
Dominik H <databasev@yahoo.de>
2014-11-20 12:54:29 +0100

    Add spezifikation.csv Test file + classes for reading in csv files.

---
+++ Apfelkuchen/src/GUI/Two/ReadCSV.java
@@ -0,0 +1,31 @@
+package GUI.Two;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+/**
+ * @author Dominik Hofmann, Mark Leibmann
+ * @version 1.0.0
+ */
+public class ReadCSV {
+	
+	public static void readCSV(){
+		ArrayList<String> content = new ArrayList<String>();
+		ReadFromFile rff = new ReadFromFile();
+		try {
+			content = rff.readFile("spezifikation.csv");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		for (int i = 1; i < content.size(); i++){ // starts at 1 so that first row is ignored
+			//System.out.println(""+content.get(i));
+	        System.out.println("-------------------------- Neue Zeile:"); 
+	        final StringTokenizer st = new StringTokenizer(content.get(i), ";"); 
+	        while (st.hasMoreTokens()) {  
+	            System.out.println("Nächste Spalte:" + st.nextToken()); 
+	        } 
+		}
+	}
+	
+}
