800e41fbf174ddedcd0d893434de178732f2fe6a
Dominik H <databasev@yahoo.de>
2014-11-23 20:39:59 +0100

    auto-populate Einheit dropdown menu with values that have been read from spezifikation.csv

--- Apfelkuchen/src/GUI/Two/Run.java
+++ Apfelkuchen/src/GUI/Two/Run.java
@@ -6,81 +6,87 @@ import java.util.Vector;
 
 /**
  * @author Yury Kalinin, Dominik Hofmann
- * @version 2.0.1 Start of window with 1194, 550 size
+ * @version 2.0.2 Start of window with 1194, 550 size
  */
 public class Run {
 	private static Vector nameOfValue;
-	private static Vector rolle;
+	private static Vector<String> rolle;
 	private static int numberOfComp = 0;
+	private static int numExp;
 	public static ArrayList Si;
 	public static List<UnitTable> unitTable = new ArrayList<UnitTable>();
-
-	private static int numExp;
-
+	
 	public static void main(String args[]) {
 		ReadCSV.readCSV("spezifikation.csv");
-		rolle = new Vector();
+		rolle = new Vector<String>();
 		setRolle();
 		numExp = 0;
 		nameOfValue = new Vector();
-
 		Window window = new Window();
 	}
-
+	
 	public static void remove() {
 		System.out.println(numberOfComp + "run comp");
 		nameOfValue.remove(numberOfComp - 1);
-
 		numberOfComp--;
 	}
-
+	
 	public static int getNumExp() {
 		return numExp;
 	}
-
+	
 	public static void changeNumExp() {
 		numExp++;
 	}
-
+	
 	public static void changeNumExp(int value) {
 		numExp = value;
 	}
-
+	
 	public static void changeNumComp() {
 		System.out.println(numberOfComp);
 		numberOfComp++;
 	}
-
+	
 	public static int getNumComp() {
 		return nameOfValue.size();
 	}
-
+	
 	public static Vector getNameOfValue() {
 		return nameOfValue;
-
 	}
-
+	
 	public static void AddNameValue(String nameValue) {
 		System.out.println(nameValue + " Run");
 		nameOfValue.add(nameValue);
 		for (int i = 1; i < nameOfValue.size(); i++) {
 			System.out.println(nameOfValue.get(i).toString());
 		}
-
 	}
-
+	
 	public static int getnameOfValueSize() {
 		return nameOfValue.size();
 	}
-
+	
 	public static void setRolle() {
-
 		rolle.add("controlled");
 		rolle.add("constant");
 		rolle.add("scale-up");
 	}
-
-	public static Vector getRolle() {
+	
+	public static Vector<String> getRolle() {
 		return rolle;
 	}
+	
+	public static Vector<String> getUnits() {
+		//FIXME duplicates are returned as well
+		Vector<String> units = new Vector<String>();
+		if (Run.unitTable.size() > 0) {
+			for (int i = 0; i < Run.unitTable.size(); i++) {
+				units.add(Run.unitTable.get(i).getUnitName());
+			}
+		}
+		return units;
+	}
+	
 }
