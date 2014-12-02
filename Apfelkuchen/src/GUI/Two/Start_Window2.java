e591132deb3022b6b2cba198c73e0147da89a9fe
ClemKre <derkoopa@gmail.com>
2014-12-02 14:20:04 +0100

    Test

--- Apfelkuchen/src/GUI/Two/Start_Window2.java
+++ Apfelkuchen/src/GUI/Two/Start_Window2.java
@@ -4,7 +4,7 @@ public class Start_Window2 {
 	public static void main(String[] args) {
 		Window2 test=new Window2(
 				new double[][]{{1.0,2.0},{-1.0,-2.0},{1.0,2.0},{-1.0,0.0},{0.0,-2.0}},//,{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0},{0.0,0.0}},
-				new String[]{"Höhe","Breite","Dichte","Temperatur","Verhältnis"},//,"Test 0","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3"},
+				new String[]{"Höhe","Breite","Dichte","Temperatur","Verhältnis"},//,"Test 1","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3","Test 2","Test 3"},
 				new String[]{"Alpha","Beta"},
 				new String[][]{{"-5","0"},{"5","10"}},
 				new String[][]{{"0","0"},{"0","0"},{"0","0"},{"0","0"},{"0","0"},{"0","0"},{"0","0"}}
