9e7e9424fc5ef5aebf645a0c439d9c5a615fe912
CWuetschner <christoph-wuetschner@gmx.de>
2014-12-09 14:29:37 +0100

    J2R und suggestVMatrix -> Getrennte Klassen

--- Apfelkuchen/src/J2R/J2R.java
+++ Apfelkuchen/src/J2R/J2R.java
@@ -1,5 +1,4 @@
 package J2R;
-
 import rcaller.RCaller;
 import rcaller.RCode;
 
@@ -8,21 +7,14 @@ public class J2R {
 	private RCode code;
 //	private String rSkriptExePfad;
 	private String rExePfad;
-	private String RskriptSuggestVmatrix;
-	private String[] colNames;
-	private double[][] vMatrix;
-	private String[] rowNames;
-	private int[] mydim;
-	static private String vMatrixResultName ="vMatrix";
-	static private String rowNamesResultName ="rowNames";
-	static private String colNamesResultName ="colNames";
-	private boolean suggestVmatrixResults;
-	private boolean resultsAvailable;
+	private String skriptPath;
+//	private String RskriptSuggestVmatrix;
+
 	
-	public J2R()
+	public J2R(String skript)
 	{
 		initRCaller("C://Program Files/R/R-3.1.1/bin/x64");//ersetzten wenn bekannt wo das her kommen wird
-		initRskript();
+		initRskript(skript);
 		initRCode();		
 	};
 	private void initRCaller(String PathPrefix)
@@ -33,99 +25,44 @@ public class J2R {
 		rExePfad=PathPrefix.concat("/R.exe");		
 		caller.setRExecutable(rExePfad);
 	}
-	private void initRskript()
+	private void initRskript(String skript)
 	{
 		//RskriptSuggestVmatrix="C://Users/Florian/Documents/RSkript.R"; //Anpassen wenn bekannt ist wo Skript(e) untergebracht werden
 		//RskriptSuggestVmatrix="C://Users/Clemens/Documents/RSkript.R"; //Anpassen wenn bekannt ist wo Skript(e) untergebracht werden
-		RskriptSuggestVmatrix="RScripts/RSkript.R"; //Anpassen wenn bekannt ist wo Skript(e) untergebracht werden
+		//RskriptSuggestVmatrix="D://Skript.R"; //Anpassen wenn bekannt ist wo Skript(e) untergebracht werden
+		skriptPath = skript;
 	}
 	private void initRCode()
 	{
-		code=new RCode();
+		code = null;
+		code = new RCode();
 		code.clear();
+		code.R_source(skriptPath);
 	}
-	public void setCodeToSuggestVmatrix(String[] abkuerzungen,double[][] dMatrix, boolean debug)
+	public void setCode(RCode Code)
 	{
-		code.clear();
-		code.R_source(RskriptSuggestVmatrix);		//wenn mehrere Skripte benutzt werden ebenfalls anpassen// bei einem Startpunkt festlegen
+		//code.clear();
+		//code.R_source(skriptPath);		//wenn mehrere Skripte benutzt werden ebenfalls anpassen// bei einem Startpunkt festlegen
 													// starten und dann weglassen // nach Run ist es vorhanden
-		code.addDoubleMatrix("D", dMatrix);
-		code.addStringArray("u_abbr", abkuerzungen);
 		
-		if(debug)
-			code.addRCode(vMatrixResultName.concat("<-suggestVmatrix(u_abbr,D,TRUE)"));
-		else
-			code.addRCode(vMatrixResultName.concat("<-suggestVmatrix(u_abbr,D,FALSE)"));
+		caller.setRCode(code);	
+		
+	}
 
-		suggestVmatrixResults=true;
-	}
-	public void startRCallerWithSuggestVmatrix()
-	{
-		if(suggestVmatrixResults)
-		{
-			caller.setRCode(code);
-			code.addRCode(vMatrixResultName.concat("<-t(vMatrix)"));
-			caller.runAndReturnResultOnline(vMatrixResultName);
-			mydim = caller.getParser().getDimensions(vMatrixResultName);
-			vMatrix=caller.getParser().getAsDoubleMatrix(vMatrixResultName, mydim[1], mydim[0]);
-			code.clear();
-			code.addRCode(rowNamesResultName.concat("<-rownames(vMatrix)"));
-			caller.runAndReturnResultOnline(rowNamesResultName);
-			rowNames=caller.getParser().getAsStringArray(rowNamesResultName);
-			code.clear();
-			code.addRCode(colNamesResultName.concat("<-colnames(vMatrix)"));
-			caller.runAndReturnResultOnline(colNamesResultName);
-			colNames=caller.getParser().getAsStringArray(colNamesResultName);
-			code.clear();	
-			resultsAvailable=true;
-		}else
-		{
-			System.out.println("Don`t forget setCodeToSuggestVmatrix");
-		}		
-		
-	}
-	public double[][] getVMatrix()
-	{
-		if(resultsAvailable)
-		{
-			return vMatrix;
-		}else{
-			return new double[0][0];
-		}
-	}
-	public String[] getRownames()
-	{
-		if(resultsAvailable)
-		{
-			return rowNames;
-		}else{
-			return new String[]{};
-		}
-	}
-	public String[] getColnames()
-	{
-		if(resultsAvailable)
-		{
-			return colNames;
-		}else{
-			return new String[]{};
-		}
-	}
-	public int[] getMydim()
-	{
-		if(resultsAvailable)
-		{
-			return mydim;
-		}else{
-			return new int[]{};
-		}
-	}
+
 	public void stopRCaller()
 	{
-		suggestVmatrixResults=false;
-		resultsAvailable=false;
+		
 		caller.stopStreamConsumers();
 		caller.StopRCallerOnline();
 		caller.deleteTempFiles();
+	}
+	public RCode getRCode()
+	{
+		return this.code;
+	}
+	public RCaller getRCaller()
+	{
+		return this.caller;
 	}
 }
--- Apfelkuchen/src/J2R/J2RTestStart.java
+++ Apfelkuchen/src/J2R/J2RTestStart.java
@@ -1,28 +1,105 @@
 package J2R;
 
+import rcaller.*;
+
 public class J2RTestStart {
+
+	private static String skriptPath = "RScripts/RSkript.R";
+	private static J2R testJ2R = new J2R(skriptPath);
+	static private String[] colNames;
+	static private double[][] vMatrix;
+	static private String[] rowNames;
+	private static int[] mydim;
+	static private String vMatrixResultName = "vMatrix";
+	static private String rowNamesResultName = "rowNames";
+	static private String colNamesResultName = "colNames";
+	private static boolean suggestVmatrixResults;
+	static private boolean resultsAvailable;
+
 	public static void main(String[] args) {
-	J2R testJ2R=new J2R();
-	testJ2R.setCodeToSuggestVmatrix(new String[]{"Test1","Test2","Test3","Test4","Test5"},
-	new double[][]{{1,2,3,4,5,6,7},{1,2,3,4,5,6,7},{1,2,3,4,5,6,7},{1,2,3,4,5,6,7},{1,2,3,4,5,6,7}} ,true);
-	testJ2R.startRCallerWithSuggestVmatrix();
-	double results[][]=testJ2R.getVMatrix();
-	String[] n=testJ2R.getRownames();
-	String[] cn=testJ2R.getColnames();
-	int mydim[]=testJ2R.getMydim();
-	testJ2R.stopRCaller();
-	
-	for (int i = 0; i < mydim[1]; i++) {
-	if (i < n.length)
-		System.out.print(n[i] + " ");
-	for (int j = 0; j < mydim[0]; j++) {
-		System.out.print(results[i][j]);// Spalte /Zeilen
-		System.out.print(" ");
+
+		System.out.println("-------------- 1test --------");
+		startRCallerWithSuggestVmatrix(setCodeToSuggestVmatrix(new String[] {
+				"Test1", "Test2", "Test3", "Test4", "Test5" },
+
+		new double[][] { { 1, 2, 3, 4, 5, 6, 7 }, { 1, 2, 3, 4, 5, 6, 7 },
+				{ 1, 2, 3, 4, 5, 6, 7 }, { 1, 2, 3, 4, 5, 6, 7 },
+				{ 1, 2, 3, 4, 5, 6, 7 } }, true));
+		System.out.println("-------------- 2test --------");
+		double results[][] = vMatrix;
+		String[] n = rowNames;
+		String[] cn = colNames;
+		testJ2R.stopRCaller();
+
+		for (int i = 0; i < mydim[1]; i++) {
+			if (i < n.length)
+				System.out.print(n[i] + " ");
+			for (int j = 0; j < mydim[0]; j++) {
+				System.out.print(results[i][j]);// Spalte /Zeilen
+				System.out.print(" ");
+			}
+
+			System.out.println(cn[i]);
+			System.out.print('\n');
+		}
 	}
-	System.out.println("das ist ein test Push");
-	System.out.println(cn[i]);
-	System.out.print('\n');
+
+	public static RCode setCodeToSuggestVmatrix(String[] abkuerzungen,
+			double[][] dMatrix, boolean debug) {
+		RCode code = testJ2R.getRCode(); // wenn mehrere Skripte benutzt
+											// werden ebenfalls anpassen//
+											// bei einem Startpunkt
+											// festlegen
+											// starten und dann weglassen //
+											// nach Run ist es vorhanden
+		code.addDoubleMatrix("D", dMatrix);
+		code.addStringArray("u_abbr", abkuerzungen);
+
+		if (debug)
+			code.addRCode(vMatrixResultName
+					.concat("<-suggestVmatrix(u_abbr,D,TRUE)"));
+		else
+			code.addRCode(vMatrixResultName
+					.concat("<-suggestVmatrix(u_abbr,D,FALSE)"));
+
+		suggestVmatrixResults = true;
+		return code;
 	}
+
+	public static void startRCallerWithSuggestVmatrix(RCode c) {
+
+		RCaller caller = testJ2R.getRCaller();
+		RCode code = c;
+		try {
+			if (suggestVmatrixResults) {
+
+				caller.setRCode(code);
+				code.addRCode(vMatrixResultName.concat("<-t(vMatrix)"));
+				System.out.println("-------------- 3test --------");
+				caller.runAndReturnResultOnline(vMatrixResultName);
+				System.out.println("-------------- 4test --------");
+				mydim = caller.getParser().getDimensions(vMatrixResultName);
+				System.out.println("-------------- 5test --------");
+				vMatrix = caller.getParser().getAsDoubleMatrix(
+						vMatrixResultName, mydim[1], mydim[0]);
+				System.out.println("-------------- 6test --------");
+				code.clear();
+				code.addRCode(rowNamesResultName.concat("<-rownames(vMatrix)"));
+				caller.runAndReturnResultOnline(rowNamesResultName);
+				rowNames = caller.getParser().getAsStringArray(
+						rowNamesResultName);
+				code.clear();
+				code.addRCode(colNamesResultName.concat("<-colnames(vMatrix)"));
+				caller.runAndReturnResultOnline(colNamesResultName);
+				colNames = caller.getParser().getAsStringArray(
+						colNamesResultName);
+				code.clear();
+				resultsAvailable = true;
+			} else {
+				System.out.println("Don`t forget setCodeToSuggestVmatrix");
+			}
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
 	}
-	//TestTestTest
 }
