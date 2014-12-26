73831b305d6d24d92027ef65c02b168b2b25c700
Sycend <Sycend@Glyph>
2014-12-26 15:24:00 +0100

    added MVC Class

---
+++ Apfelkuchen/src/GUI/Two/GUI1Model.java
@@ -0,0 +1,5 @@
+package GUI.Two;
+
+public class GUI1Model {
+
+}
---
+++ Apfelkuchen/src/GUI/Two/ModelViewController.java
@@ -0,0 +1,19 @@
+package GUI.Two;
+
+public class ModelViewController {
+	private GUI1View _view;
+	private GUI1Model _model;
+	
+	public GUI1Controler() {
+		this._model = new GUI1Model();
+		this._view =new GUI1View;
+		
+		addListener();
+	}
+	public void ShowView(){
+		this._view.setVisible(true);
+	}
+	private void addListener()(){
+		this._view.
+	}
+}
