2250dd5fb0b5bc6c912d492d398efb220f909319
CWuetschner <christoph-wuetschner@gmx.de>
2015-01-17 18:42:08 +0100

    Preparden MinMax and SuggestVMatrix for usage with GUI

--- Apfelkuchen/src/J2R/MinMaxTestMainFürController.java
+++ Apfelkuchen/src/J2R/MinMaxTestMainFürController.java
@@ -4,56 +4,59 @@ import rcaller.RCode;
 
 public class MinMaxTestMainFürController {
 
-	public static void main(String[] args) {
-		J2R vMatrix =  J2R.getInstance();
+	private J2R callerInstance = J2R.getInstance();
+	private MinMax minMax;
+
+	public MinMaxTestMainFürController(double[][] VMatrix,
+			String[] rowNamesArray, String[] colNamesArray,
+			double[] u_lowArray, double[] u_highArray) {
+
 		
-		//Bei Controllerübernahme ersetzten
-		
-		double [][] VMatrix = new double [][] {{-3,-1},{0,1},{1,0},{3,0},{1,0}}; 
-		String [] rowNamesArray = new String [] {"d", "h", "Te", "ti", "alp"};
-		String [] colNamesArray = new String [] {"PI1", "PI2"};
-		double [] u_lowArray = new double [] {-1,-2,-3,-4,-5};
-		double [] u_highArray = new double [] {1,2,3,4,5};
-				
-		MinMax minMax = new MinMax(VMatrix, rowNamesArray, colNamesArray,u_lowArray, u_highArray, vMatrix.getRCode());
-		
-		
-		
+		minMax = new MinMax(VMatrix, rowNamesArray, colNamesArray, u_lowArray,
+				u_highArray, callerInstance.getRCode());
+
+		runMinMax();
+
+	}
+
+	public void runMinMax() {
+
 		RCode code = new RCode();
 		code = minMax.MinMaxCode();
-		vMatrix.setCode(code);
-		vMatrix.runAndReturnResultOnline(minMax.getRunandReturnOnlineString());
-		
-		
-		
-		double [] x_low = vMatrix.getParser().getAsDoubleArray(minMax.getX_lowDoubleArray());
-		double [] x_high = vMatrix.getParser().getAsDoubleArray(minMax.getX_highDoubleArray());
-		
-		String [] x_lowColNames = vMatrix.getParser().getAsStringArray(minMax.getX_lowColnamesStringArray());
-		String [] x_lowRowNames =vMatrix.getParser().getAsStringArray(minMax.getX_lowRownamesStringArray());
-		
-		String [] x_highColNames = vMatrix.getParser().getAsStringArray(minMax.getX_highColnamesStringArray());
-		String [] x_highRowNames = vMatrix.getParser().getAsStringArray(minMax.getX_highRownamesStringArray());
-		
-		
-		vMatrix.stopRCaller();
-		
-		// Test Ausgabe (Bei übernahme nicht nötig)
-		
-		for(int i = 0; i < x_low.length; i++){
-			System.out.print(x_lowRowNames[i]);
-			System.out.print(" ");
-			System.out.print(x_low[i]);
-			System.out.println("");
-			System.out.print(x_highRowNames[i]);
-			System.out.print(" ");
-			System.out.print(x_high[i]);
-			System.out.println("");
-			
-		}
-			System.out.println(x_lowColNames[0]);
-			System.out.println(x_highColNames[0]);
+		callerInstance.setCode(code);
+		callerInstance.runAndReturnResultOnline(minMax
+				.getRunandReturnOnlineString());
 
+	}
+
+	public double[] getX_lowDoubleArray() {
+		return callerInstance.getParser().getAsDoubleArray(
+				minMax.getX_lowDoubleArray());
+	}
+
+	public String[] getX_lowColnamesStringArray() {
+		return callerInstance.getParser().getAsStringArray(
+				minMax.getX_lowColnamesStringArray());
+	}
+
+	public String[] getX_lowRownamesStringArray() {
+		return callerInstance.getParser().getAsStringArray(
+				minMax.getX_lowRownamesStringArray());
+	}
+
+	public double[] getX_highDoubleArray() {
+		return callerInstance.getParser().getAsDoubleArray(
+				minMax.getX_highDoubleArray());
+	}
+
+	public String[] getX_highColnamesStringArray() {
+		return callerInstance.getParser().getAsStringArray(
+				minMax.getX_highColnamesStringArray());
+	}
+
+	public String[] getX_highRownamesStringArray() {
+		return callerInstance.getParser().getAsStringArray(
+				minMax.getX_highRownamesStringArray());
 	}
 
 }
--- Apfelkuchen/src/J2R/SingeltonTestMainStart.java
+++ Apfelkuchen/src/J2R/SingeltonTestMainStart.java
@@ -4,6 +4,8 @@ public class SingeltonTestMainStart {
 
 	public static void main(String[] args) {
 
+		//Beispiel
+		//---------------------------------------------------------
 		String[] u_roles = { "contr", "contr", "contr", "contr", "contr" };
 		String[] role = { "Controlled" };
 		String[] colNames = { "m", "k", "s", "kel", "mol", "amp", "cand" };
@@ -13,7 +15,10 @@ public class SingeltonTestMainStart {
 				{ 0, 0, 1, 0, 0, 0, 0 }, { 3, 0, -3, -1, 0, 0, 0 } };
 
 		boolean debug = true;
-
+		//--------------------------------------------------------------
+		
+		
+		
 		J2R callerInstance = J2R.getInstance();
 		VMatrixControllerMain SuggestVMatrix = new VMatrixControllerMain(
 				callerInstance, u_roles, dMatrix, colNames, rowNames, role,
@@ -40,7 +45,40 @@ public class SingeltonTestMainStart {
 		}
 
 		// -------------------------------------------------------------------------------------------
-		//
+		
+		//Beispiel
+		//---------------------------------------------------------
+		double[] u_lowArray = new double[] { -1, -2, -3, -4, -5 };
+		double[] u_highArray = new double[] { 1, 2, 3, 4, 5 };
+		
+		//---------------------------------------------------------
+		
+		MinMaxTestMainFürController minMax = new MinMaxTestMainFürController(results, rn, cn, u_lowArray, u_highArray);
+		
+		double[] x_low = minMax.getX_lowDoubleArray();
+		double[] x_high =  minMax.getX_highDoubleArray();
+
+		String[] x_lowColNames =  minMax.getX_lowColnamesStringArray();
+		String[] x_lowRowNames =  minMax.getX_lowRownamesStringArray();
+
+		String[] x_highColNames =  minMax.getX_highColnamesStringArray();
+		String[] x_highRowNames =  minMax.getX_highRownamesStringArray();
+
+		// Test Ausgabe (Bei übernahme nicht nötig)
+
+		for (int i = 0; i < x_low.length; i++) {
+			System.out.print(x_lowRowNames[i]);
+			System.out.print(" ");
+			System.out.print(x_low[i]);
+			System.out.println("");
+			System.out.print(x_highRowNames[i]);
+			System.out.print(" ");
+			System.out.print(x_high[i]);
+			System.out.println("");
+
+		}
+		System.out.println(x_lowColNames[0]);
+		System.out.println(x_highColNames[0]);
 
 		callerInstance.stopRCaller();
 
