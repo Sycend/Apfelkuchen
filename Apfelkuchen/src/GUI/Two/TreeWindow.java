e7ccd8332a7c5b0826844e90a1645aec88d44882
jklinin <yuri.kalinin@outlook.de>
2015-01-29 15:55:27 +0100

    fixed bug with bgCl for Unit and Dimension

--- Apfelkuchen/src/GUI/Two/TreeWindow.java
+++ Apfelkuchen/src/GUI/Two/TreeWindow.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -124,5 +126,41 @@ public class TreeWindow extends JFrame {
 				}
 			}
 		});
-	}
+	
+	
+	this.addWindowListener(new WindowListener() {
+		@Override
+		public void windowOpened(WindowEvent e) {
+		}
+		
+		@Override
+		public void windowIconified(WindowEvent e) {
+		}
+		
+		@Override
+		public void windowDeiconified(WindowEvent e) {
+		}
+		
+		@Override
+		public void windowDeactivated(WindowEvent e) {
+		}
+		
+		@Override
+		public void windowClosing(WindowEvent e) {
+			textFieldDimension.setBackground(Color.WHITE); 
+			textFieldUnit.setBackground(Color.WHITE);		
+		}
+		
+		@Override
+		public void windowClosed(WindowEvent e) {
+		}
+		
+		@Override
+		public void windowActivated(WindowEvent e) {
+		}
+	});
+	
+		
+	
+}
 }
